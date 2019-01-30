fb2e7937fd95b5cf9b12227c5703eadca6430f1f
Giacomo Mariotti <giajj@hotmail.com>
2019-01-30 09:24:03 +0000

    Multi-language evaluation

--- tests/sample_requests.py
+++ tests/sample_requests.py
@@ -72,7 +72,7 @@ sample_facebook_text_request = {
    "session": "projects/pristine-atom-226517/agent/sessions/6fa3c1fe-6573-48b4-8ad1-d9ce2e71fc27"
 }
 
-sample_facebook_flac_request = {
+sample_facebook_flac_request_en = {
    "responseId": "e5439946-24f5-4995-85be-4f36b9d96b3a",
    "queryResult": {  
       "queryText": "FACEBOOK_MEDIA",
@@ -144,7 +144,7 @@ sample_facebook_flac_request = {
    "session": "projects/pristine-atom-226517/agent/sessions/7128851b-8967-49f6-8f04-8a86ca97137d"
 }
 
-sample_facebook_opus_request = {  
+sample_facebook_opus_request_en = {
    "responseId": "959c96e0-7ffd-46d0-9055-13204e0c9033",
    "queryResult": {  
       "queryText": "FACEBOOK_MEDIA",
@@ -214,4 +214,77 @@ sample_facebook_opus_request = {
       }
    },
    "session": "projects/pristine-atom-226517/agent/sessions/f35afb09-42c4-45c1-8b49-ff7d4c066409"
+}
+
+
+sample_facebook_opus_request_it = {  
+   "responseId": "4317f8d5-5fbe-41b2-a80c-020e25066114",
+   "queryResult": {  
+      "queryText": "FACEBOOK_MEDIA",
+      "action": "input.unknown",
+      "parameters": {  
+
+      },
+      "allRequiredParamsPresent": True,
+      "fulfillmentMessages": [  
+         {  
+            "text": {  
+               "text": [  
+                  ""
+               ]
+            }
+         }
+      ],
+      "outputContexts": [  
+         {  
+            "name": "projects/pristine-atom-226517/agent/sessions/250c2e62-5c94-49f6-a2f6-fd3d04fb78b1/contexts/facebook_media"
+         },
+         {  
+            "name": "projects/pristine-atom-226517/agent/sessions/250c2e62-5c94-49f6-a2f6-fd3d04fb78b1/contexts/defaultfallbackintent-followup",
+            "lifespanCount": 2
+         },
+         {  
+            "name": "projects/pristine-atom-226517/agent/sessions/250c2e62-5c94-49f6-a2f6-fd3d04fb78b1/contexts/generic",
+            "lifespanCount": 4,
+            "parameters": {  
+               "facebook_sender_id": "2109732695780744"
+            }
+         }
+      ],
+      "intent": {  
+         "name": "projects/pristine-atom-226517/agent/intents/d4032e6d-d2a7-4659-931e-f12ef17ca2da",
+         "displayName": "Default Fallback Intent",
+         "isFallback": True
+      },
+      "intentDetectionConfidence": 1.0,
+      "languageCode": "en"
+   },
+   "originalDetectIntentRequest": {  
+      "source": "facebook",
+      "payload": {  
+         "data": {  
+            "sender": {  
+               "id": "2109732695780744"
+            },
+            "recipient": {  
+               "id": "590108701452104"
+            },
+            "message": {  
+               "attachments": [  
+                  {  
+                     "payload": {  
+                        "url": "https://cdn.fbsbx.com/v/t59.2708-21/51190382_597970917343490_2531532000041369600_n.opus/AUDIO-2019-01-30-08-45-16.opus?_nc_cat=111&_nc_ht=cdn.fbsbx.com&oh=d2317597f77e9076e183adcdc3fd3cfd&oe=5C54116C"
+                     },
+                     "type": "file"
+                  }
+               ],
+               "mid": "xqvUYZ9i5ieyHE8KyaRtkaIP7U1RzGN2MLu2WlPcaUUL5zoyuR97GAGvfH5LC8U1J_aiG75XD38ZINAS62EESg",
+               "seq": 24889.0
+            },
+            "timestamp": 1548837992892.0
+         },
+         "source": "facebook"
+      }
+   },
+   "session": "projects/pristine-atom-226517/agent/sessions/250c2e62-5c94-49f6-a2f6-fd3d04fb78b1"
 }
